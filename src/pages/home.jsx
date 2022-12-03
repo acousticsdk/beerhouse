589ce3c7c888bd4d1be6a228d51e101f5bdfbd1e
adm <acoustic.sdk@gmail.com>
2022-12-03 17:14:41 +0200

    last upd

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -10,15 +10,18 @@ import axios from 'axios'
 import { AppContext } from '../App'
 import { useSelector, useDispatch } from 'react-redux'
 import { setCategoryId, setFilter } from '../redux/slices/filterSlice'
+import { setProducts } from '../redux/slices/productsSlice'
 
 function Home() {
     // const navigate = useNavigate()
     const dispatch = useDispatch()
+    const items = useSelector((state) => state.products.items)
+
     const categoryId = useSelector((state) => state.filter.categoryId)
     const sort = useSelector((state) => state.filter.sort.sortProperty)
 
     const { searchValue } = useContext(AppContext)
-    const [items, setItems] = useState([])
+    // const [items, setItems] = useState([])
     const [loaded, setLoaded] = useState(false)
     // const isSearch = useRef(false)
     // const isMounted = useRef(false)
@@ -32,20 +35,27 @@ function Home() {
 
     useEffect(() => {
         const fetchProducts = async () => {
-            const order = sort.includes('-') ? 'desc' : 'asc'
-            const sortBy = sort.replace('-', '')
-            const category = categoryId > 0 ? `&category=${categoryId}` : ''
-            const search =
-                searchValue.length !== 0 ? `&search=${searchValue}` : ''
+            try {
+                const order = sort.includes('-') ? 'desc' : 'asc'
+                const sortBy = sort.replace('-', '')
+                const category = categoryId > 0 ? `&category=${categoryId}` : ''
+                const search =
+                    searchValue.length !== 0 ? `&search=${searchValue}` : ''
 
-            const response = await axios.get(
-                `https://63456567dcae733e8ff13e95.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}${search}`
-            )
-            setItems(response.data)
-            setLoaded(true)
-            window.scrollTo(0, 0)
+                const response = await axios.get(
+                    `https://63456567dcae733e8ff13e95.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}${search}`
+                )
+                dispatch(setProducts(response.data))
+
+                window.scrollTo(0, 0)
+            } catch (error) {
+                alert(error.message)
+            } finally {
+                setLoaded(true)
+            }
         }
         setLoaded(false)
+
         fetchProducts()
     }, [categoryId, sort, searchValue])
 
